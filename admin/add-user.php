c2f64106395465f70c3b0d7f4327c0fce93f7cec
khalid-aptech <khalid@aptechnorth.edu.pk>
2022-09-30 15:59:24 +0500

    initial commit

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,8 +1,8 @@
 <head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
 
@@ -13,57 +13,6 @@
             <div class="col-md-12">
                 <h1 class="admin-heading">Add User</h1>
             </div>
-
-
-            <?php
-            ini_set('display_errors', 0);
-
-
-            $user_fname = $_POST["fname"];
-            $user_lname = $_POST["lname"];
-            $user_name = $_POST["user"];
-            $user_password = $_POST["password"];
-            $user_role = $_POST["role"];
-
-            $conn = mysqli_connect("localhost","root","","kstore");
-
-            $query = "SELECT * from `user` WHERE `username` = '{$user_name}'";
-
-            $result = mysqli_query($conn,$query);
-
-            if(mysqli_num_rows($result)>0)
-            {
-                echo "user already exist";
-
-            }
-            else
-            {
-                $conn = mysqli_connect("localhost","root","","kstore");
-
-                $query1 = "INSERT INTO `user`(`first_name`, `last_name`, `username`, `password`, `role`) VALUES ('{$user_fname}','{$user_lname}','{$user_name}','{$user_password}','{$user_role}')";
-
-                mysqli_query($conn,$query1);
-
-                header("location:http://localhost:82/kj/admin/users.php");
-
-            }
-
-
-        
-      
-
-
-
-
-
-
-            ?>
-
-
-
-
-
-
             <div class="col-md-offset-3 col-md-6">
                 <!-- Form Start -->
                 <form action="<?php $_SERVER["PHP_SELF"] ?>" method="POST" autocomplete="off">
@@ -94,6 +43,42 @@
                     <input type="submit" name="save" class="btn btn-primary" value="Save" required />
                 </form>
                 <!-- Form End-->
+
+                <?php
+
+                if (isset($_POST["save"])) {
+
+
+                    $user_fname = $_POST["fname"];
+                    $user_lname = $_POST["lname"];
+                    $user_name = $_POST["user"];
+                    $user_password = $_POST["password"];
+                    $user_role = $_POST["role"];
+
+                    $conn = mysqli_connect("localhost", "root", "", "kstore");
+
+                    $query = "SELECT * from `user` WHERE `username` = '{$user_name}'";
+
+                    $result = mysqli_query($conn, $query);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        echo "user already exist";
+                    } else {
+                        $conn = mysqli_connect("localhost", "root", "", "kstore");
+
+                        $query1 = "INSERT INTO `user`(`first_name`, `last_name`, `username`, `password`, `role`) VALUES ('{$user_fname}','{$user_lname}','{$user_name}','{$user_password}','{$user_role}')";
+
+                        mysqli_query($conn, $query1);
+
+                        header("location:http://localhost:82/k_store/admin/users.php");
+                    }
+                }
+
+
+                ?>
+
+
+
             </div>
         </div>
     </div>
